149c5f3b01512636eab8e720b065dedd3156e1e5
chaipetchp <dekwidwa_tum@hotmail.com>
2015-02-21 18:34:53 +0700

    'commit'

---
+++ include/footer.php
@@ -0,0 +1,75 @@
+<div class="footer_bg">
+    <div class="wrap">
+        <div class="wrapper">
+            <div class="clear">
+                <div class="f_grid">
+                    <div class="grid1_of_1">
+                        <div class="address">
+                            <p>p +64 9555 555</p>
+                            <p>F +64 9555 555</p>
+                            <p>E <a href="">info@email.com</a> </p>
+                            <h3 class="top">Address</h3>
+                            <p class="top">Template by <a href="http://w3layouts.com">w3layouts</a></p>
+                        </div>
+                    </div>
+                    <div class="grid1_of_2">
+                        <div class="f_grid2 left">
+                            <h3>Services</h3>
+                            <ul class="f_nav">
+                                <li><a href="">Commercial</a></li>
+                                <li><a href="">Distrubution</a></li>
+                                <li><a href="">Gas</a></li>
+                                <li><a href="">Generation</a></li>
+                                <li><a href="">Industrial</a></li>
+                                <li><a href="">Telecomms</a></li>
+                                <li><a href="">Transmition</a></li>
+                            </ul>
+                        </div>
+                        <div class="f_grid2 left">
+                            <h3>Markets</h3>
+                            <ul class="f_nav">
+                                <li><a href="">Commercial</a></li>
+                                <li><a href="">Industrial</a></li>
+                                <li><a href="">Infrastructure</a></li>
+                                <li><a href="">Resources</a></li>
+                                <li><a href="">Utilities</a></li>
+                            </ul>
+                        </div>
+                        <div class="f_grid2 left">
+                            <h3>HSEWQ</h3>
+                            <ul class="f_nav">
+                                <li><a href="">Health &</a></li>
+                                <li><a href="">Safety</a></li>
+                                <li><a href="">Environment</a></li>
+                                <li><a href="">Quality</a></li>
+                            </ul>
+                        </div>
+                        <div class="f_grid2 left hide">
+                            <h3>About</h3>
+                            <ul class="f_nav">
+                                <li><a href="">Histroy</a></li>
+                                <li><a href="">Structure</a></li>
+                                <li><a href="">People</a></li>
+                                <li><a href="">CSR</a></li>
+                                <li><a href="">Locations</a></li>
+                                <li><a href="">PPV</a></li>
+                            </ul>
+                        </div>
+                        <div class="f_grid2 left hide">
+                            <h3>Careers</h3>
+                            <ul class="f_nav">
+                                <li><a href="">Who we are</a></li>
+                                <li><a href="">Benefits</a></li>
+                                <li><a href="">Programmes</a></li>
+                                <li><a href="">Our People</a></li>
+                                <li><a href="">Roles/</a></li>
+                                <li><a href="">Vacancies</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ include/header.php
@@ -0,0 +1,42 @@
+<div class="wrap">
+    <div class="wrapper">
+        <div class="logo">
+            <a href="index.php"><img src="images/logo.png" alt=""/> </a>
+        </div>
+        <div class="menu">
+            <a class="toggleMenu" href="#">Menu</a>
+            <ul class="nav">
+                <li><a href="commercial.php">Commercial</a>
+                    <ul>
+                        <li><a href="#">Histroy</a></li>
+                        <li><a href="#">Structure</a></li>
+                        <li><a href="#">People</a></li>
+                    </ul>
+                </li>
+                <li><a href="industrial.php">Industrial</a>
+                    <ul>
+                        <li><a href="#">CSR</a></li>
+                        <li><a href="#">Locations</a></li>
+                        <li><a href="#">PPV</a></li>
+                    </ul>
+                </li>
+                <li><a href="infrastructure.php">infrastructure</a>
+                    <ul>
+                        <li><a href="#">Histroy</a></li>
+                        <li><a href="#">Structure</a></li>
+                        <li><a href="#">People</a></li>
+                    </ul>
+                </li>
+                <li><a href="resource.php">Resources</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+        <div class="clear"></div>
+        <script type="text/javascript" src="js/script.js"></script>
+        <div class="clear"></div>
+        <div class="banner">
+            <h2 class="style">i want to feel What you wanted</h2>
+            <p class="para">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+        </div>
+    </div>
+</div>
